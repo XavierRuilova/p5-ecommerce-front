0109b5aed0f105612db542246b3be76ab431423a
Xavier Ruilova <xaruilova.dwfsc12@bootcamp.ucamp.io>
2023-04-20 16:08:26 -0500

    scrollbar in store created

---
+++ src/components/showProd/ShowProd.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowProd = (props) => {
+    const {idProd, pic, title, description, price} = props
+
+  return (
+    <div>ShowProd</div>
+  )
+}
+
+export default ShowProd
--- src/pages/store/Store.js
+++ src/pages/store/Store.js
@@ -7,36 +7,36 @@ import CardList from "../../components/cardlist/CardList";
 import "./store.css";
 
 const Store = () => {
-    const ctx = useContext(AppContext)
-  const { getProducts, products } = ctx
+  const ctx = useContext(AppContext);
+  const { getProducts, products } = ctx;
 
-  useEffect( () => {
-     getProducts();
-
+  useEffect(() => {
+    getProducts();
   }, []);
 
-  const data = ()=>{
-    return products.map((item)=>{
-      return{
-          idprod: item._id,
-          imgurl: item.imgurl,
-          productname: item.productname,
-          price: `${item.price} USD`,
-      }
-  })
-  }
-  
-
-    return (
-      <>
-        <Header />
-        <section className="slider-container">
-            <CardList list={products ? data():[]}/>
-        </section>
-
-        <Footer />
-      </>
-    );
+  const data = () => {
+    return products.map((item) => {
+      return {
+        idprod: item._id,
+        imgurl: item.imgurl,
+        productname: item.productname,
+        price: `${item.price} USD`,
+      };
+    });
   };
+
+  return (
+    <>
+      <Header />
+
+      <div style={{ display: "flex" }}>
+        <section className="slider-container">
+          <CardList list={products ? data() : []} />
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Store;
